ac8759276409a8c6767329219dda50600b9e4810
samuele delpero <samuele.delpero@mail.polimi.it>
2022-03-22 16:06:26 +0100

    Prova push

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -6,8 +6,8 @@ package it.polimi.ingsw;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println("Samu");
     }
 }

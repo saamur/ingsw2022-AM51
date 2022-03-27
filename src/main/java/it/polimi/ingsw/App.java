78e958968f0ffdcdfe46166cc2bee4408f34709d
Giulia Cornetta <giulia.cornetta@mail.polimi.it>
2022-03-27 12:08:21 +0200

    added changes to repository

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -5,6 +5,7 @@ package it.polimi.ingsw;
  *
  */
 public class App {
+    //TODO delete class
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("Eryantis");

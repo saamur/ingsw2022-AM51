ef0d502105327bd5c696c03a0058b562cd4215dd
Giulia Cornetta <giulia.cornetta@mail.polimi.it>
2022-03-31 19:02:53 +0200

    update HallTest

--- src/test/java/it/polimi/ingsw/HallTest.java
+++ src/test/java/it/polimi/ingsw/HallTest.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.player.Hall;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HallTest {
     /**

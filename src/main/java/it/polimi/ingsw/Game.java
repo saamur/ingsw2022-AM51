bc50ecc30647b75db59222f120a3dc57dcc9ce87
samuele delpero <samuele.delpero@mail.polimi.it>
2022-04-03 10:48:01 +0100

    Add javadoc to Game class

--- src/main/java/it/polimi/ingsw/Game.java
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -20,11 +20,11 @@ import java.util.function.Function;
 
 public class Game implements GameInterface {
 
-    private static final Function<Player, Integer>[] winningScoreCalculators = new Function[2];
+    private static final Function<Player, Integer>[] WINNING_SCORE_CALCULATORS = new Function[2];
 
     static {
-        winningScoreCalculators[0] = p -> (-1) * p.getNumberOfTowers();
-        winningScoreCalculators[1] = p -> {
+        WINNING_SCORE_CALCULATORS[0] = p -> (-1) * p.getNumberOfTowers();
+        WINNING_SCORE_CALCULATORS[1] = p -> {
             int numProfessors = 0;
             for (Clan c : Clan.values())
                 if (p.getChamber().hasProfessor(c))
@@ -96,6 +96,11 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method addPlayer adds a Player to the Game if there isn't already another with the same nickname
+     * @param nickname  the nickname that will be assigned to the new Player
+     * @return          whether the nickname wasn't already used and the Player was consequently added
+     */
     @Override
     public boolean addPlayer (String nickname) {
 
@@ -124,6 +129,9 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method start selects a random player to begin the game and calls initRound
+     */
     private void start() {
 
         Random random = new Random();
@@ -133,6 +141,10 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method initRound calls calculateWin if lastRound is true, otherwise it sets gameState to PIANIFICATION
+     * and fills the Clouds
+     */
     private void initRound() {
 
         if (lastRound) {
@@ -151,6 +163,14 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method sets currCard of the currentPlayer to card if the game phase is correct,
+     * the choice is valid and playerNickname is the nickname of the current Player.
+     * If every Player has chosen a Card the method calls createOrderActionPhase and initActionPhase
+     * @param playerNickname    the nickname of the Player that called this method
+     * @param card              the chosen Card
+     * @return                  whether currCard of the current Player was set to card or not
+     */
     @Override
     public boolean chosenCard (String playerNickname, Card card) {
 
@@ -183,6 +203,13 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method validCard calculates whether a Player can use a certain Card
+     * based on the Cards used in this round by the other Players
+     * @param player    the Player for which the method determines if the choice is valid
+     * @param card      the Card chosen
+     * @return          whether the choice is valid
+     */
     private boolean validCard (Player player, Card card) {
 
         List<Card> otherPlayersCards = new ArrayList<>();
@@ -201,6 +228,10 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method createOrderActionPhase fills the attribute playersOrder and the attribute nextFirstPlayer
+     * based on the cards chosen by the Players in this round
+     */
     private void createOrderActionPhase() {             //TODO needs deep testing
 
         List<Player> order = new ArrayList<>();
@@ -229,6 +260,10 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method initActionPhase sets gameState to ACTION, sets indexCurrPlayer to 0
+     * and instantiates a Turn for the first Player of this phase
+     */
     private void initActionPhase() {
 
         gameState = GameState.ACTION;
@@ -237,6 +272,12 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method moveStudentToChamber moves a student of the specified Clan from the Hall to the Chamber of the current Player
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param clan              the Clan of the student to move
+     * @return                  whether the move was valid and the student was actually moved
+     */
     @Override
     public boolean moveStudentToChamber (String playerNickname, Clan clan) {
 
@@ -251,6 +292,14 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method moveStudentToChamber moves a student of the specified Clan from the Hall of the current Player
+     * to the Island with the specified index
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param clan              the Clan of the student to move
+     * @param islandIndex       the index of the Island on which to move the student
+     * @return                  whether the move was valid and the student was actually moved
+     */
     @Override
     public boolean moveStudentToIsland (String playerNickname, Clan clan, int islandIndex) {
 
@@ -266,6 +315,14 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method moveMotherNature moves Mother Nature on the Island with the specified index,
+     * calls checkInfluence and updates the state of the Turn.
+     * If it's the last round it calls endTurn
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param islandIndex       the index of the Island on which to move Mother Nature
+     * @return                  whether the move is valid and has been carried out
+     */
     @Override
     public boolean moveMotherNature (String playerNickname, int islandIndex) {
 
@@ -294,6 +351,13 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method checkInfluence, if the given Island doesn't contain any prohibition card,
+     * updates the controlling Player of that Island performing all its consequences
+     * and check if a winning condition is met.
+     * If the Island does contain a prohibition card it is removed and reassigned to the corresponding characterCard
+     * @param island    the Island on which the controlling Player is updated
+     */
     public void checkInfluence (Island island) {             //FIXME we have to find a better name
 
         if (island.getNumProhibitionCards() > 0) {
@@ -314,16 +378,13 @@ public class Game implements GameInterface {
 
     }
 
-    private void reassignProhibitionCard() {
-        for (CharacterCard card : availableCharacterCards) {
-            if (card.getCharacterID() == CharacterID.GRANDMA) {                 //FIXME better with instanceof?
-                ProhibitionCharacterCard c = (ProhibitionCharacterCard) card;
-                c.addProhibitionCard();
-                break;
-            }
-        }
-    }
-
+    /**
+     * method chosenCloud, if the choice is valid, adds the students on the Cloud with the given index
+     * to the Hall of the current Player
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param cloudIndex        the index of the chosen Cloud
+     * @return                  whether the move is valid and has been carried out
+     */
     @Override
     public boolean chosenCloud (String playerNickname, int cloudIndex) {
 
@@ -346,6 +407,10 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method endTurn calls InitRound if the current Player is the last one of this round phase,
+     * otherwise it instantiates a new Turn for the next Player
+     */
     private void endTurn() {
 
         indexCurrPlayer++;
@@ -360,13 +425,17 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method calculateWin sets the winners variable with the List of the winning Players
+     * and sets gameState to GAME_OVER
+     */
     private void calculateWin() {
 
         gameState = GameState.GAME_OVER;
 
         List<Player> potentialWinners = new ArrayList<>(Arrays.asList(players));
 
-        for (Function<Player, Integer> f : winningScoreCalculators) {
+        for (Function<Player, Integer> f : WINNING_SCORE_CALCULATORS) {
 
             int i = 0;
             while(i+1 < potentialWinners.size()) {
@@ -397,12 +466,19 @@ public class Game implements GameInterface {
     }
 
 
-
+    /**
+     * method activateCharacterCard, if the move is valid,
+     * activates the CharacterCard with that CharacterID for the current turn
+     * and performs any potential initial effect
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param characterID       the CharacterID of the CharacterCard to activate
+     * @return                  whether the move is valid and the CharacterCard has been activated
+     */
     @Override
     public boolean activateCharacterCard (String playerNickname, CharacterID characterID) {
 
         Player player = playerFromNickname(playerNickname);
-        CharacterCard characterCard = CharacterCardFromID(characterID);
+        CharacterCard characterCard = characterCardFromID(characterID);
 
         if (player == null || characterCard == null)
             return false;
@@ -432,6 +508,13 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method applyCharacterEffect applies the effect of the CharacterCard currently active
+     * on the Island with the specified index
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param islandIndex       the index of the Island on which the effect has to be carried out
+     * @return                  whether the move is valid and has been carried out
+     */
     @Override
     public boolean applyCharacterCardEffect (String playerNickname, int islandIndex) {
 
@@ -463,6 +546,13 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method setClanCharacter calls the method setCharacterClan on the current Turn, setting a Clan variable
+     * necessary for some CharacterCards to perform their effects
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param clan              the Clan chosen by the Player
+     * @return                  whether the move was valid and has been carried out
+     */
     @Override
     public boolean setClanCharacter (String playerNickname, Clan clan) {
 
@@ -487,6 +577,14 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method applyCharacterEffect applies the effect of the CharacterCard currently active with the given parameters
+     * @param playerNickname    the nickname of the Player that requested this move
+     * @param destination       the destination of the students moved (if necessary)
+     * @param students1         the students to move to the destination (if necessary)
+     * @param students2         the students to move from the destination, in case of an exchange of students (if necessary)
+     * @return                  whether the move was valid and has been carried out
+     */
     @Override
     public boolean applyCharacterCardEffect (String playerNickname, StudentContainer destination, int[] students1, int[] students2) {
 
@@ -520,12 +618,41 @@ public class Game implements GameInterface {
 
     }
 
+    /**
+     * method reassignProhibitionCard find the characterCard that can accept prohibition cards and adds it to it
+     */
+    private void reassignProhibitionCard() {
+        for (CharacterCard card : availableCharacterCards) {
+            if (card.getCharacterID() == CharacterID.GRANDMA) {                 //FIXME better with instanceof?
+                ProhibitionCharacterCard c = (ProhibitionCharacterCard) card;
+                c.addProhibitionCard();
+                break;
+            }
+        }
+    }
 
 
+    /**
+     * method playerFromNickname finds the Player with the given nickname and returns it
+     * @param nickname  the nickname of the Player to find
+     * @return          the Player with the given nickname, if present, null otherwise
+     */
     private Player playerFromNickname (String nickname) {
         for (Player p : players)
             if(p.getNickname().equals(nickname))
                 return p;
+        return null;
+    }
+
+    /**
+     * method characterCardFromID finds the characterCard with that with the given CharacterID and returns it
+     * @param characterID   the CharacterID of the CharacterCard to find
+     * @return              the CharacterCard with the given CharacterID, if available, null otherwise
+     */
+    private CharacterCard characterCardFromID(CharacterID characterID) {
+        for (CharacterCard c : availableCharacterCards)
+            if (c.getCharacterID() == characterID)
+                return c;
         return null;
     }
 
@@ -546,13 +673,6 @@ public class Game implements GameInterface {
             return players[indexCurrPlayer];
         if (gameState == GameState.ACTION)
             return playersOrder[indexCurrPlayer];
-        return null;
-    }
-
-    private CharacterCard CharacterCardFromID (CharacterID characterID) {
-        for (CharacterCard c : availableCharacterCards)
-            if (c.getCharacterID() == characterID)
-                return c;
         return null;
     }
 

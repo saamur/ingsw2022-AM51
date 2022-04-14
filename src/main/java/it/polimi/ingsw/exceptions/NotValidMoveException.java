c45140dc08d97c731e7346477bf05b6bb0949c29
Giulia Cornetta <giulia.cornetta@mail.polimi.it>
2022-04-14 22:46:26 +0200

    update NotValidMoveException

--- src/main/java/it/polimi/ingsw/exceptions/NotValidMoveException.java
+++ src/main/java/it/polimi/ingsw/exceptions/NotValidMoveException.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.exceptions;
 
 /**
  * The NotValidMoveException is thrown when the current player tries to make any move not legal
- * accordingly with teh game rules
+ * accordingly with the game rules
  *
  */
 public class NotValidMoveException extends Exception {

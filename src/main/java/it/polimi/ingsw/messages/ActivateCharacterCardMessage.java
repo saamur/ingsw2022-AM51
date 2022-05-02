5ebb1526fe1de4c764bea4f91202e5521b127829
Federica <federica.delbeato@mail.polimi.it>
2022-05-02 17:08:21 +0200

    update javadocs

--- src/main/java/it/polimi/ingsw/messages/ActivateCharacterCardMessage.java
+++ src/main/java/it/polimi/ingsw/messages/ActivateCharacterCardMessage.java
@@ -3,6 +3,11 @@ package it.polimi.ingsw.messages;
 import it.polimi.ingsw.model.charactercards.CharacterID;
 
 public record ActivateCharacterCardMessage(CharacterID characterCardID) implements Message{
+
+    /**
+     *
+     * @return
+     */
     @Override
     public String getMessage(){
         return this.toString();

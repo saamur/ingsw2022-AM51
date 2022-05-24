b3c51db3074297e83be133313136185bd91d5278
Giulia Cornetta <giulia.cornetta@mail.polimi.it>
2022-05-24 14:41:26 +0200

    update Client

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -14,27 +14,31 @@ import static java.lang.Thread.currentThread;
 public class Client {
 
     public static void main(String[] args){
-        System.out.println("Welcome to the Eriantys board game!");
-        System.out.println("First of all choose if you want to use the cli or the gui");
+        if(args.length != 0) {
+            System.out.println("Welcome to the Eriantys board game!");
+            System.out.println("First of all choose if you want to use the cli or the gui");
 
 
-        Scanner scanner = new Scanner(System.in);
-        String choice = scanner.nextLine().toUpperCase();
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.nextLine().toUpperCase();
 
-        if ("CLI".equals(choice)) {
-            CLI cli = new CLI();
-            Thread cliThread = new Thread(cli);
-            cliThread.start();
-        }
-        else if ("GUI".equals(choice)) {
+            if ("CLI".equals(choice)) {
+                CLI cli = new CLI();
+                Thread cliThread = new Thread(cli);
+                cliThread.start();
+            } else if ("GUI".equals(choice)) {
+                GUI gui = new GUI();
+                gui.launchGUI();
+                System.exit(0);
+            } else {
+                System.out.println("This choice is not valid");
+                System.out.println("The game will close");
+                System.exit(-1);
+            }
+        } else {
             GUI gui = new GUI();
             gui.launchGUI();
             System.exit(0);
-        }
-        else {
-            System.out.println("This choice is not valid");
-            System.out.println("The game will close");
-            System.exit(-1);
         }
 
 

64a2e568c1f0a15feb75fcba1dd075620ab1afa5
Giulia Cornetta <giulia.cornetta@mail.polimi.it>
2022-05-30 23:05:00 +0200

    update GameConstants

--- src/main/java/it/polimi/ingsw/constants/GameConstants.java
+++ src/main/java/it/polimi/ingsw/constants/GameConstants.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.model.charactercards.CharacterID;
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.*;
 
+import static it.polimi.ingsw.model.charactercards.CharacterID.*;
+import static it.polimi.ingsw.model.charactercards.CharacterID.THIEF;
+
 /**
  * GameConstants class contains the main constants used in the Erianys game
  *
@@ -102,6 +105,28 @@ public class GameConstants {
 
     public static int getNumInitialStudentsStudentMoverCharacterCard (CharacterID characterID) {
         return NUM_INITIAL_STUDENTS_STUDENT_MOVER_CHARACTER_CARD.get(characterID);
+    }
+
+    private static final Map<CharacterID, String> DESCRIPTION_CHARACTERS;
+
+    static{
+        DESCRIPTION_CHARACTERS = new EnumMap<>(CharacterID.class);
+        DESCRIPTION_CHARACTERS.put(MONK, "Prendi 1 studente dalla carta e piazzalo su un'Isola a tua scelta\nPoi, pesca 1 Studente del sacchetto e mettilo su questa carta");
+        DESCRIPTION_CHARACTERS.put(FARMER, "Durante questo turno, prendi il controllo dei Professori anche se nella tua sala hai lo stesso numero di studenti del giocatore che li controlla in quel momento");
+        DESCRIPTION_CHARACTERS.put(HERALD, "Scegli un'Isola e calcola la maggioranza come se Madre Natura avesse terminato il suo movimento lì.\nIn questo turno Madre Natura si muoverà come di consueto e nell'Isola dove terminerà il suo movimento la maggioranza verrà normalmente calcolata");
+        DESCRIPTION_CHARACTERS.put(POSTMAN, "Puoi muovere Madre Natura fino a 2 Isole addizionali rispetto a quanto indicato sulla carta Assistente che hai giocato");
+        DESCRIPTION_CHARACTERS.put(GRANDMA, "Piazza una Tessera Divieto su un'Isola a tua scelta.\nLa prima volta che Madre Natura termina il suo movimento lì, rimettete la Tessera Divieto sulla carta senza calcolare l'influenza su quest'Isola nel piazzare Torri");
+        DESCRIPTION_CHARACTERS.put(CENTAUR, "Durante il conteggio dell'influenza su un'Isola (o su un gruppo di Isole), le Torri presenti non vengono calcolate");
+        DESCRIPTION_CHARACTERS.put(JESTER, "Puoi prendere fino a 3 studenti da questa carta e scambiarli con altrettanti studenti presenti nel tuo ingresso");
+        DESCRIPTION_CHARACTERS.put(KNIGHT, "In questo turno durante il calcolo dell'influenza hai due punti di influenza addizionali");
+        DESCRIPTION_CHARACTERS.put(MUSHROOMPICKER, "Scegli il colore di uno Studente; in questo turno, durante il calcolo dell'influenza quel colore non fornisce influenza");
+        DESCRIPTION_CHARACTERS.put(MINSTREL, "Puoi scambiare fra loro fino a 2 studenti presenti nella tua sala e nel tuo ingresso");
+        DESCRIPTION_CHARACTERS.put(PRINCESS, "Prendi uno studente da questa carta e piazzalo nella tua sala. Poi pesca uno studente dal sacchetto e posizionalo su questa carta");
+        DESCRIPTION_CHARACTERS.put(THIEF, "Scegli il colore di uno Studente; ogni giocatore (incluso te) deve rimettere nel sacchetto 3 Studenti di quel colore presenti nella tua sala. Chi avesse meno di 3 studenti di quel colore, rimetterà tutti quelli che ha");
+    }
+
+    public static String getDescriptionCharacter(CharacterID character){
+        return DESCRIPTION_CHARACTERS.get(character);
     }
 
 }
